97bcb39691d3171ce626c0a7665abcca9a6130f3
talhak52 <talhak52@gmail.com>
2023-12-28 22:01:40 +0530

    a

--- music_store.sql
+++ music_store.sql
@@ -1,3 +1,5 @@
+/*	Question Set 1 - Easy */
+
 -- Q1: senior most employee based on job title
 select * from employee
 order by levels desc
@@ -31,6 +33,40 @@ on iv.customer_id=cx.customer_id
 group by cx.customer_id, cx.first_name
 order by total desc
 limit 1;
+
+/* Question Set 2 - Moderate */
+
+/* Q1: Write query to return the email, first name, last name, & Genre of all Rock Music listeners. 
+Return your list ordered alphabetically by email starting with A. */
+
+SELECT DISTINCT email AS Email,first_name AS FirstName, last_name AS LastName, genre.name AS Name
+FROM customer
+JOIN invoice ON invoice.customer_id = customer.customer_id
+JOIN invoice_line ON invoice_line.invoice_id = invoice.invoice_id
+JOIN track ON track.track_id = invoice_line.track_id
+JOIN genre ON genre.genre_id = track.genre_id
+WHERE genre.name LIKE 'Rock'
+ORDER BY email;
+
+/* Q2: Let's invite the artists who have written the most rock music in our dataset. 
+Write a query that returns the Artist name and total track count of the top 10 rock bands.*/
+
+select ar.artist_id, ar.name, count(ar.artist_id) as number_of_songs
+from artist ar
+join album al on al.artist_id = ar.artist_id
+join track tr on tr.album_id = al.album_id
+join genre gn on gn.genre_id = tr.genre_id
+where gn.name like 'rock' 
+group by ar.artist_id, ar.name
+order by number_of_songs desc;
+
+
+
+
+
+
+
+
 
 
 
