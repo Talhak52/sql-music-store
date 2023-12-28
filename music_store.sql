2adf11a8d203a8941dc76071077b66c575843e88
talhak52 <talhak52@gmail.com>
2023-12-28 17:27:18 +0530

    my sql project

--- music_store.sql
+++ music_store.sql
@@ -2,3 +2,32 @@
 select * from employee
 order by levels desc
 limit 1;
+-- countrie have the most invoices
+select count(*), billing_country
+from invoice
+group by billing_country
+order by count(*) desc
+limit 1;
+-- top 3 unique values of total invoice
+select distinct total
+from invoice 
+order by total desc
+limit 3;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
